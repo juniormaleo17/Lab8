576a260448464976007dcca299f3255a5e26cd72
Junior Maleo <jmaleomi@my.centennialcollege.ca>
2018-07-11 16:36:25 -0400

    initial commit

---
+++ Scripts/core/app.js
@@ -0,0 +1,39 @@
+// core module - IIFE
+(function() {
+  // App variables
+  let clickMeButton;
+  let ButtonSection;
+  let firstH1;
+
+  /**
+   * This function is used for Intialization
+   */
+  function Start() {
+    console.log(
+      `%c App Initializing...`,
+      "font-weight: bold; font-size: 20px; color:blue;"
+    );
+
+    clickMeButton = new objects.Button("/Assets/images/ClickMeButton.png", 200, 50);
+    ButtonSection = document.getElementById("ButtonSection");
+    firstH1 = document.getElementsByTagName("h1")[0];
+
+    Main();
+  }
+
+  /**
+   * This function is the where the main functionality for our
+   * web app is happening
+   */
+  function Main() {
+    console.log(`%c App Started...`, "font-weight: bold; font-size: 20px; color:green;");
+
+    ButtonSection.appendChild(clickMeButton);
+
+    clickMeButton.addEventListener("click", function(){
+      firstH1.textContent = "clickMeButton Clicked!";
+    });
+  }
+
+  window.addEventListener("load", Start);
+})();
